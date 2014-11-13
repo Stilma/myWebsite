f203ac3e603225145e9d161e07a9c326dd70fdab
Stilma <anneta_s@ukr.net>
2014-11-13 16:48:50 +0200

    call compare

--- Game_RockPaperScissors.js
+++ Game_RockPaperScissors.js
@@ -1,6 +1,6 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
-console.log(computerChoice);
+
 if(0<=computerChoice<=0.33){
     computerChoice = 'rock';
 }else if(0.34<=computerChoice<=0.66){
@@ -21,3 +21,5 @@ var compare = function(choice1,choice2){
         }
     }
 };
+
+compare(userChoice,computerChoice);
